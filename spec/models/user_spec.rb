e812c2871e5076711c0fb56b5c2f75ba6256f446
Joseph Lee <jhjlee702@gmail.com>
2024-01-14 17:57:20 -0700

    write callback test for user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,12 @@ describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_confirmation_of(:password) }
   end
+
+  describe "callbacks" do
+    it "should generate an api_key before creating a user" do
+      user = User.create(email: "test@test.com", password: "123", password_confirmation: "123")
+
+      expect(user.api_key).to be_present
+    end
+  end
 end
