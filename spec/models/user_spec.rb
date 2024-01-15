b39d9ef7ad4771266d76f10165c4492495ab2b7a
Joseph Lee <jhjlee702@gmail.com>
2024-01-14 17:53:05 -0700

    add shoulda matchers to rails_helper and write user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:email) } 
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
 end
