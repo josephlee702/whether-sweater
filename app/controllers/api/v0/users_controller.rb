5eca53164a2802657efb33398fe2b419250182c6
Joseph Lee <jhjlee702@gmail.com>
2024-01-14 18:24:51 -0700

    add simplecov and finish user_request_spec.rb

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -2,15 +2,16 @@ class Api::V0::UsersController < ApplicationController
   def create
     user = User.new(user_params)
 
-    if user.save
-      render json: format_user_response(user)
-    elsif User.exists?(email: user.email)
+    if User.exists?(email: user.email)
       render json: email_already_exists(user)
     elsif user.password != user.password_confirmation
       render json: passwords_dont_match(user)
+    elsif user.save
+      render json: format_user_response(user)
     else
     end
   end
+  
 
   private
 
