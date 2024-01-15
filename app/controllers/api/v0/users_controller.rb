96213f614f3cd702a2b1fd84cfc57ddd7f5e5142
Joseph Lee <jhjlee702@gmail.com>
2024-01-14 20:47:01 -0700

    finish US3

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -11,8 +11,19 @@ class Api::V0::UsersController < ApplicationController
     else
     end
   end
-  
 
+  def log_in
+    #going against the security tip of not telling what field is incorrect because it narrows a hacker's scope of work, just because I've seen this tip shown and I also think it is helpful for users.
+    #however, I can see why we would want to avoid this and give a more general error message. Just due to it's safety, I'd probably follow the security tip provided but for now, I will keep it like this.
+    if !User.exists?(email: params[:email])
+      render json: no_account_found
+    elsif user = User.find_by(email: params[:email])&.authenticate(params[:password])
+      render json: successful_log_in(user)
+    else
+      render json: wrong_password
+    end
+  end
+    
   private
 
   def user_params
@@ -46,6 +57,36 @@ class Api::V0::UsersController < ApplicationController
     {
       error_message: "The passwords do not match.",
       status: 422
+    }
+  end
+
+  def successful_log_in(user)
+    {
+      status: 200,
+      body: {
+        data: {
+          type: 'users',
+          id: user.id,
+          attributes: {
+            email: user.email,
+            api_key: user.api_key
+          }
+        }
+      }
+    }
+  end
+
+  def no_account_found
+    {
+      status: 404,
+      error_message: "There is no account associated with this email."
+    }
+  end
+
+  def wrong_password
+    {
+      status: 404,
+      error_message: "The password is incorrect for this account."
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v0 do
       get "/forecast", to: "locations#show"
       post "/users", to: "users#create"
+      post "/sessions", to: "users#log_in"
     end
   end
 end
