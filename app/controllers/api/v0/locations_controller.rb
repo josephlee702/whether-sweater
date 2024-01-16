471c1023657f395b786a209f43c2ed8cc42504eb
Joseph Lee <jhjlee702@gmail.com>
2024-01-16 09:09:49 -0700

    rewrite weather and mapquest facades and services. also rewrite forecasts#show

--- app/controllers/api/v0/locations_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V0::LocationsController < ApplicationController
-  def show
-    data = MapquestFacade.get_lat_lon(params[:location])
-    lat = data[:results].first[:locations].first[:latLng][:lat]
-    lon = data[:results].first[:locations].first[:latLng][:lng]
-    forecast = WeatherFacade.get_forecast(lat,lon)
-    render json: WeatherSerializer.format_forecast(forecast)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      get "/forecast", to: "locations#show"
+      get "/forecast", to: "forecasts#show"
     end
   end
 end
