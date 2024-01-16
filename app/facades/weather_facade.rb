20c69c5654cd5c85824169f77305c13556700b22
Joseph Lee <jhjlee702@gmail.com>
2024-01-15 20:25:19 -0700

    initial changes based off feedback to weather service/facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,5 +1,6 @@
 class WeatherFacade
   def self.get_forecast(lat, lon)
-    WeatherService.get_url("?key=#{Rails.application.credentials.weather_api[:key]}&q=#{lat},#{lon}&aqi=no&days=5")
+   service = WeatherService.get_forecast(lat, lon)
+   
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -7,4 +7,8 @@ class WeatherService
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_forecast(lat, lon)
+    get_url("?key=#{Rails.application.credentials.weather_api[:key]}&q=#{lat},#{lon}&aqi=no&days=5")
+  end
 end
