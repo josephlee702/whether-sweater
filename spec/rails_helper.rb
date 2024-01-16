9b7355f3dcdb45d9d2133435748e47cf539472bb
Joseph Lee <jhjlee702@gmail.com>
2024-01-16 09:10:49 -0700

    add VCR gem and rails_helper block

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,3 +64,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { record: :new_episodes }
+  config.configure_rspec_metadata!
+end
