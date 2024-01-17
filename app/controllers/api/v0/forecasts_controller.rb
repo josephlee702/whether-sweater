c6361516651519da0c1333760c218e19ca28c329
Joseph Lee <jhjlee702@gmail.com>
2024-01-16 18:32:40 -0700

    change params q to location

--- app/controllers/api/v0/forecasts_controller.rb
+++ app/controllers/api/v0/forecasts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V0::ForecastsController < ApplicationController
   def show
-    render json: WeatherFacade.get_forecast(params[:q])
+    render json: WeatherFacade.get_forecast(params[:location])
   end
 end
