8487e14fae00d1d4dbe09210061be1cd7574eaed
Joseph Lee <jhjlee702@gmail.com>
2024-01-16 17:49:26 -0700

    finish user story 4

--- app/facades/directions_facade.rb
+++ app/facades/directions_facade.rb
@@ -1,5 +1,79 @@
 class DirectionsFacade
   def self.get_route(from, to, api_key)
-    DirectionsService.get_url("?key=#{api_key}&from=#{from}&to=#{to}")
+    forecast = WeatherFacade.get_forecast(to)
+
+    directions_data = DirectionsService.get_route(from, to, api_key)
+    directions = directions_data[:route]
+
+    format_directions(forecast, directions)
+  end
+
+  private
+
+  def self.format_directions(forecast, directions)
+    {
+      "data": {
+        "id": nil,
+        "type": "road_trip",
+        "attributes": {
+          "start_city": "#{directions[:locations].first[:adminArea5]}, #{directions[:locations].first[:adminArea3]}",
+          "end_city": "#{directions[:locations].second[:adminArea5]}, #{directions[:locations].first[:adminArea3]}",
+          "travel_time": self.travel_time_from_seconds(directions[:realTime]), 
+          "weather_at_eta": {
+            "datetime": datetime_at_eta(directions),
+            "temperature": temperature_at_eta(forecast, directions),
+            "condition": condition_at_eta(forecast, directions)
+          }
+        }
+      }
+    }
+  end
+
+  private
+
+  def self.travel_time_from_seconds(seconds)
+    hours = seconds / 3600
+    minutes = (seconds % 3600) / 60
+    seconds = seconds % 60
+
+    "%02d:%02d:%02d" % [hours, minutes, seconds]
+  end
+
+  def self.datetime_at_eta(directions)
+    current_time = Time.now
+    seconds_to_add = directions[:realTime]
+
+    time = current_time + seconds_to_add
+    formatted_rounded_time = time.strftime("%Y-%m-%d %H:%M")
+  end
+
+  def self.temperature_at_eta(forecast, directions)
+    eta = datetime_at_eta(directions)
+    eta_time = Time.parse(eta)
+
+    rounded_time = Time.at((eta_time.to_f / 3600).round * 3600)
+    formatted_rounded_time = rounded_time.strftime("%Y-%m-%d %H:%M")
+
+    forecast[:data][:attributes][:hourly_weather].each do |hour|
+      if hour[:time] == formatted_rounded_time
+        return hour[:temperature]
+      else
+      end
+    end
+  end
+
+  def self.condition_at_eta(forecast, directions)
+    eta = datetime_at_eta(directions)
+    eta_time = Time.parse(eta)
+
+    rounded_time = Time.at((eta_time.to_f / 3600).round * 3600)
+    formatted_rounded_time = rounded_time.strftime("%Y-%m-%d %H:%M")
+
+    forecast[:data][:attributes][:hourly_weather].each do |hour|
+      if hour[:time] == formatted_rounded_time
+        return hour[:conditions]
+      else
+      end
+    end
   end
 end
